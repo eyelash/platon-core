5b8d0dd5c909221e427bf915a991a807a64c67a2
Elias Aebi <eyelash@users.noreply.github.com>
2018-11-24 16:06:03 +0100

    json

--- editor.hpp
+++ editor.hpp
@@ -1,27 +1,7 @@
 #pragma once
 
 #include "buffer.hpp"
-#include <string>
-
-class JSONString: public std::string {
-	static constexpr std::size_t strlen(const char* s, std::size_t i = 0) {
-		return *s == '\0' ? i : strlen(s + 1, i + 1);
-	}
-public:
-	void push_back_string(const char* first, const char* last) {
-		push_back('"');
-		for (; first != last; ++first) {
-			const char c = *first;
-			if (c == '"' || c == '\\')
-				push_back('\\');
-			push_back(c);
-		}
-		push_back('"');
-	}
-	void push_back_string(const char* s) {
-		push_back_string(s, s + strlen(s));
-	}
-};
+#include "json.hpp"
 
 class Editor {
 	Buffer buffer;
@@ -32,25 +12,22 @@ public:
 		return newlines.get_total_lines();
 	}
 	const char* render(std::size_t first_line, std::size_t last_line) const {
-		static JSONString json;
+		static std::string json;
 		json.clear();
-		json.push_back('[');
-		for (std::size_t i = first_line; i < last_line; ++i) {
-			if (i > first_line) json.push_back(',');
-			json.push_back('{');
-			json.push_back_string("text");
-			json.push_back(':');
-			if (i < get_total_lines()) {
-				const std::size_t index0 = newlines.get_index(i);
-				const std::size_t index1 = newlines.get_index(i + 1);
-				json.push_back_string(buffer.begin() + index0, buffer.begin() + index1);
+		JSONWriter writer(json);
+		writer.write_array([&](JSONArrayWriter& writer) {
+			for (std::size_t i = first_line; i < last_line; ++i) {
+				writer.write_element().write_object([&](JSONObjectWriter& writer) {
+					std::size_t index0 = 0;
+					std::size_t index1 = 0;
+					if (i < get_total_lines()) {
+						index0 = newlines.get_index(i);
+						index1 = newlines.get_index(i + 1);
+					}
+					writer.write_member("text").write_string(buffer.begin() + index0, buffer.begin() + index1);
+				});
 			}
-			else {
-				json.push_back_string("");
-			}
-			json.push_back('}');
-		}
-		json.push_back(']');
+		});
 		return json.c_str();
 	}
 };
---
+++ json.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+class JSONWriter {
+	std::string& string;
+	static constexpr std::size_t strlen(const char* s, std::size_t i = 0) {
+		return *s == '\0' ? i : strlen(s + 1, i + 1);
+	}
+public:
+	JSONWriter(std::string& string): string(string) {}
+	void write_string(const char* first, const char* last) {
+		string.push_back('"');
+		for (; first != last; ++first) {
+			const char c = *first;
+			if (c == '"' || c == '\\')
+				string.push_back('\\');
+			string.push_back(c);
+		}
+		string.push_back('"');
+	}
+	void write_string(const char* s) {
+		write_string(s, s + strlen(s));
+	}
+	void write_number(std::int64_t n) {
+		if (n < 0) {
+			string.push_back('-');
+			n = -n;
+		}
+		if (n >= 10) {
+			write_number(n / 10);
+		}
+		string.push_back('0' + (n % 10));
+	}
+	template <class F> void write_object(F f) {
+		string.push_back('{');
+		ObjectWriter object_writer(*this);
+		f(object_writer);
+		string.push_back('}');
+	}
+	template <class F> void write_array(F f) {
+		string.push_back('[');
+		ArrayWriter array_writer(*this);
+		f(array_writer);
+		string.push_back(']');
+	}
+	class ObjectWriter {
+		JSONWriter& writer;
+		bool object_start;
+	public:
+		ObjectWriter(JSONWriter& writer): writer(writer), object_start(true) {}
+		JSONWriter& write_member(const char* name) {
+			if (object_start)
+				object_start = false;
+			else
+				writer.string.push_back(',');
+			writer.write_string(name);
+			writer.string.push_back(':');
+			return writer;
+		}
+	};
+	class ArrayWriter {
+		JSONWriter& writer;
+		bool array_start;
+	public:
+		ArrayWriter(JSONWriter& writer): writer(writer), array_start(true) {}
+		JSONWriter& write_element() {
+			if (array_start)
+				array_start = false;
+			else
+				writer.string.push_back(',');
+			return writer;
+		}
+	};
+};
+
+using JSONObjectWriter = JSONWriter::ObjectWriter;
+using JSONArrayWriter = JSONWriter::ArrayWriter;
