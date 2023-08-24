d5e45e0d9de2ad028fb823de5ca91ba77be00619
Elias Aebi <eyelash@users.noreply.github.com>
2023-08-24 10:20:14 +0200

    Python syntax improvements

--- languages/python.hpp
+++ languages/python.hpp
@@ -3,6 +3,12 @@ constexpr auto python_comment = sequence('#', repetition(but('\n')));
 constexpr auto python_syntax = repetition(choice(
 	// comments
 	highlight(Style::COMMENT, python_comment),
+	// literals
+	highlight(Style::WORD, highlight(Style::LITERAL, c_keywords(
+		"None",
+		"False",
+		"True"
+	))),
 	// keywords
 	highlight(Style::WORD, highlight(Style::KEYWORD, c_keywords(
 		"lambda",
@@ -21,5 +27,7 @@ constexpr auto python_syntax = repetition(choice(
 		"def",
 		"class"
 	))),
+	// identifiers
+	highlight(Style::WORD, sequence(c_identifier_begin_char, zero_or_more(c_identifier_char))),
 	any_char()
 ));
